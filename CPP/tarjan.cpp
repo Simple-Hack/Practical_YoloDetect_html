917992bb27edd5bd7e025d924ce0c9522d375b6e
starDust <GanYu_996@outlook.com>
2024-04-16 21:51:44 +0800

    牛马，数据大到离谱，每一步都要求余，longlong都不够大

--- CPP/tarjan.cpp
+++ CPP/tarjan.cpp
@@ -1,47 +1,108 @@
 #include<vector>
 #include<iostream>
 #include<string>
-using namespace std;
+const long long N =1e5+10;
 
-#define MAX 10005
-#define ll long long
+long long p;
+long long a[N];
 
-vector<ll> g[MAX];
-ll color[MAX], vis[MAX], stack[MAX], dfn[MAX], low[MAX], cnt[MAX];
-//deep:节点编号 top：栈顶  sum：强连通分量数目
-ll deep, top, sum, res = 0;
+struct node {
+	long long l,r;
+	long long sum,add,mul;
+}tr[4*N];
 
-void tanjan(ll v) {
-	dfn[v] = ++deep;
-	low[v] = deep;   //(1)初始化dfn数组，同时将low设置为相同值
-	vis[v] = 1;
-	stack[++top] = v;//(2)入栈，作为栈顶元素，同时更新vis数组
+inline void pushup(long long x){
+	tr[x].sum=(tr[x*2].sum+tr[x*2+1].sum)%p;
+}
 
-	for (unsigned i = 0; i < g[v].size(); i++) {//(3)遍历所有可能到达的点
-		ll id = g[v][i];
-		if (!dfn[id]) {//如果这个点从没访问过，则先放问它，再用它更新low[v]的值
-			tanjan(id);
-			low[v] = min(low[v], low[id]); //他的儿子如果能连到更小的祖先节点，显然他也可以
-		}
-		else {
-			if (vis[id]) {//不在栈中的点，要么没有访问，要么不能到达id，所以只需要判断栈中的
-				low[v] = min(low[v], low[id]);
-			}
-		}
+void pushdown(long long x){
+	tr[x*2].sum=(tr[x*2].sum*tr[x].mul+tr[x].add*(tr[x*2].r-tr[x*2].l+1))%p;
+	tr[x*2].mul=(tr[x*2].mul*tr[x].mul)%p;
+	tr[x*2].add=(tr[x*2].add*tr[x].mul+tr[x].add)%p;
+
+	tr[x*2+1].sum=(tr[x*2+1].sum*tr[x].mul+tr[x].add*(tr[x*2+1].r-tr[x*2+1].l+1))%p;
+	tr[x*2+1].mul=(tr[x*2+1].mul*tr[x].mul)%p;
+	tr[x*2+1].add=(tr[x*2+1].add*tr[x].mul+tr[x].add)%p;
+
+	tr[x].add=0;
+	tr[x].mul=1;
+}
+
+void update(long long x,long long l,long long r,long long mul,long long add){
+	if(l<=tr[x].l && tr[x].r<=r){
+		tr[x].sum=(tr[x].sum*mul+add*(tr[x].r-tr[x].l+1))%p;
+		tr[x].mul=(tr[x].mul*mul)%p;
+		tr[x].add=(tr[x].add*mul+add)%p;
 	}
-
-	if (low[v] == dfn[v]) {//(4)自己和子节点形成了强连通分量，或者只有自己孤身一人
-		color[v] = ++sum;
-		vis[v] = 0;
-		while (stack[top] != v) {//将从v开始所有的点取出
-			color[stack[top]] = sum;//给予同一颜色
-			vis[stack[top--]] = 0;//出栈要顺便修改vis
+	else{
+		pushdown(x);
+		long long mid=(tr[x].r+tr[x].l)/2;
+		if(l<=mid){
+			update(x*2,l,r,mul,add);;
 		}
-		top--;
+		if(r>mid){
+			update(x*2+1,l,r,mul,add);
+		}
+		pushup(x);
 	}
 }
 
-int main(){
+long long query(long long x,long long l,long long r){
+	if(tr[x].l>=l && tr[x].r<=r){
+		return tr[x].sum;
+	}
+	pushdown(x);
+	long long mid=(tr[x].r+tr[x].l)/2;
+	long long s=0;
+	if(l<=mid){
+		s+=query(x*2,l,r)%p;
+	}
+	if(r>mid){
+		s+=query(x*2+1,l,r)%p;
+	}
+	return s;
+}
 
+void build(long long x,long long l,long long r){
+	tr[x].add=0;
+	tr[x].l=l;
+	tr[x].r=r;
+	tr[x].mul=1;
+	if(l==r){
+		tr[x].sum=a[l];
+	}else{
+		long long mid=(l+r)/2;
+		build(x*2,l,mid);
+		build(x*2+1,mid+1,r);
+		pushup(x);
+	}
+}
+
+int main(void){
+	long long n;
+	std::cin>>n>>p;
+	for(long long i=1;i<=n;i++){
+		std::cin>>a[i];
+	}
+	build(1,1,n);
+	long long m;
+	std::cin>>m;
+	long long q,t,g;
+	while(m--){
+		std::cin>>q>>t>>g;
+		if(q==1){
+			long long c;
+			std::cin>>c;
+			update(1,t,g,c,0);
+		}
+		else if(q==2){
+			long long c;
+			std::cin>>c;
+			update(1,t,g,1,c);
+		}
+		else{
+			std::cout<<query(1,t,g)%p<<std::endl;
+		}
+	}
 	return 0;
 }
