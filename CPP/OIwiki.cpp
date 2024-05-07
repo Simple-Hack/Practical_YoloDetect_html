3fbe9ced41078ff16fa4fd7f21802be13337c265
starDust <GanYu_996@outlook.com>
2024-05-07 11:03:15 +0800

    牛马，线段树解决不同的种类数，read函数有问题

--- CPP/OIwiki.cpp
+++ CPP/OIwiki.cpp
@@ -1,89 +1,109 @@
 #include<iostream>
 #include<algorithm>
-#include<cmath>
+#include<vector>
+const int MAXN=1e6+10;
+#define Ri register int 
+int a[MAXN],last[MAXN],ans[MAXN];
+struct Tree{
+    int l,r,sum;
+}tr[MAXN<<2];
 
+struct Node{
+    int l,r,id;
+    bool operator<(const Node& a) {return r<a.r;}
 
-const int MAXN =1e6+114;
-
-int array[MAXN],cnt[MAXN],belong[MAXN];
-int n,m,size,bnum,now,ans[MAXN];
-
-class query{
-    public:
-        int l,r,id;
 }q[MAXN];
 
-int cmp(query q1, query q2){
-    return (belong[q1.l] ^ belong[q2.l]) ? belong[q1.l]<belong[q2.l] 
-     :((belong[q1.l] & 1) ? q1.l<q2.l : q1.l>q2.l);
+inline void pushup(int x){
+    tr[x].sum=tr[x<<1].sum+tr[x<<1 | 1].sum;
 }
 
-int read(){
-    int x=0,w=1;
-    char ch=getchar();
-    while(ch < '0' || ch >'9'){
-        if(ch == '-') w=-1;
-        ch=getchar();
+inline void build(int x,int l,int r){
+    tr[x]={l,r,1};
+    if(l==r) return;
+    int mid=(l+r)>>1;
+    build(x<<1,l,mid);
+    build(x<<1 | 1,mid+1,r);
+    pushup(x);
+}
+
+inline void update(int x,int index){
+    if(tr[x].l==tr[x].r){
+        tr[x].sum=0;
+        return;
     }
-    while('0'<= ch && ch<= '9'){
-        x=x*10+ch-'0';
+    int mid=(tr[x].l+tr[x].r)>>1;
+    if(index<=mid) update(x<<1,index);
+    else if(index>mid) update(x<<1 | 1,index);
+    pushup(x);
+}
+
+inline int query(int x,int l,int r){
+    if(l>tr[x].r || r<tr[x].l) return 0;
+    if(tr[x].l>=l && tr[x].r<=r){
+        return tr[x].sum;
+    }
+    int res=0,mid;
+    mid=(tr[x].l+tr[x].r)>>1;
+    if(l<=mid) res+=query(x<<1,l,r);
+    if(r> mid) res+=query(x<<1 | 1,l,r);
+    return res;
+}
+
+
+inline int read(){
+    int x=0,w=1;
+    char ch=0;
+    while(ch<'0' || ch>'9'){
+        if(ch=='-'){
+            w=-1;
+            ch=getchar();
+        }
+    }
+    while(ch>='0' && ch<='9'){
+        x=x*10+(ch-'0');
         ch=getchar();
     }
     return x*w;
 }
 
-void write(int x){
-    static int sta[35];
+inline void write(int x){
+    int sta[35];
     int top=0;
     do{
         sta[top++]=x%10;
-        x /= 10;
+        x/=10;
     }while(x);
-    while(top){
-        putchar(sta[--top]+'0');
-    }
-}
-
-void add(int pos){
-    if(!(cnt[array[pos]]++)) now++;
-}
-
-void del(int pos){
-    cnt[array[pos]]--;
-    if(!cnt[array[pos]]) now--;
+    while(top) putchar(sta[--top]+'0');
 }
 
 int main(void){
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
+    int n,m;
     n=read();
-    size=std::sqrt(n);
-    bnum=int((n+size-1)/size);
-    for(int i=1;i<=bnum;i++){
-        for(int j=(i-1)*size+1;j<=i*size;j++){
-            belong[j]=i;
-        }
-    }
-    for(int i=1;i<=n;i++){
-        array[i]=read();
+    for(register int i=1;i<=n;i++){
+        a[i]=read();
     }
     m=read();
-    for(int i=1;i<=m;i++){
+    for(register int i=1;i<=m;i++){
         q[i].l=read();
         q[i].r=read();
         q[i].id=i;
     }
-    std::sort(q+1,q+1+m,cmp);
-    int l=1,r=0;
-    for(int i=1;i<=m;i++){
-        int ql=q[i].l,qr=q[i].r;
-        while(l<ql) del(l++);
-        while(l>ql) add(--l);
-        while(r<qr) add(++r);
-        while(r>qr) del(r--);
-        ans[q[i].id]=now;
+    std::sort(q+1,q+m+1);
+
+    build(1,1,n);
+    int index=1;
+    for(register int  i=1;i<=n;i++){
+        if(last[a[i]]){
+            update(1,last[a[i]]);
+        }
+        last[a[i]]=i;
+        while(q[index].r==i){
+            ans[q[index].id]=query(1,q[index].l,q[index].r);
+            index++;
+        }
     }
-    for(int i=1;i<=m;i++){
+    for(register int i=1;i<=m;i++){
         write(ans[i]);
         putchar('\n');
     }
