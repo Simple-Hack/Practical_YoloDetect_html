4fe9a4c95f610d57ff0b5f78d71510abc5f0b337
starDust <GanYu_996@outlook.com>
2024-05-24 14:34:10 +0800

    受不了了

--- CPP/OIwiki.cpp
+++ CPP/OIwiki.cpp
@@ -1,111 +1,51 @@
-#include<iostream>
-#include<algorithm>
-#include<vector>
-const int MAXN=1e6+10;
-#define Ri register int 
-int a[MAXN],last[MAXN],ans[MAXN];
-struct Tree{
-    int l,r,sum;
-}tr[MAXN<<2];
+#include<bits/stdc++.h> 
 
-struct Node{
-    int l,r,id;
-    bool operator<(const Node& a) {return r<a.r;}
-
-}q[MAXN];
-
-inline void pushup(int x){
-    tr[x].sum=tr[x<<1].sum+tr[x<<1 | 1].sum;
-}
-
-inline void build(int x,int l,int r){
-    tr[x]={l,r,1};
-    if(l==r) return;
-    int mid=(l+r)>>1;
-    build(x<<1,l,mid);
-    build(x<<1 | 1,mid+1,r);
-    pushup(x);
-}
-
-inline void update(int x,int index){
-    if(tr[x].l==tr[x].r){
-        tr[x].sum=0;
-        return;
-    }
-    int mid=(tr[x].l+tr[x].r)>>1;
-    if(index<=mid) update(x<<1,index);
-    else if(index>mid) update(x<<1 | 1,index);
-    pushup(x);
-}
-
-inline int query(int x,int l,int r){
-    if(l>tr[x].r || r<tr[x].l) return 0;
-    if(tr[x].l>=l && tr[x].r<=r){
-        return tr[x].sum;
-    }
-    int res=0,mid;
-    mid=(tr[x].l+tr[x].r)>>1;
-    if(l<=mid) res+=query(x<<1,l,r);
-    if(r> mid) res+=query(x<<1 | 1,l,r);
-    return res;
-}
-
-
+using namespace std;
+const int N=114;
+int a[N];
+int ans[N];
+int sum=0;
+bool vis[N]={false};
+int cnt=0;
 int read(){
-    int x=0,w=1;
-    char ch=0;
-    while(ch<'0' || ch>'9'){
-        if(ch=='-'){
+    int w=1,x=0;
+    char c=0;
+    while(!isdigit(c)){
+        if(c=='-'){
             w=-1;
         }
-        ch=getchar();
+        c=getchar();
     }
-    while(ch>='0' && ch<='9'){
-        x=x*10+(ch-'0');
-        ch=getchar();
+    while(isdigit(c)){
+        x=x*10+c-48;
+        c=getchar();
     }
-    return x*w;
+    return w*x;
 }
-
-inline void write(int x){
-    int sta[35];
-    int top=0;
-    do{
-        sta[top++]=x%10;
-        x/=10;
-    }while(x);
-    while(top) putchar(sta[--top]+'0');
-}
-
-int main(void){
-    int n,m;
+int main( )
+{   
+    a[0]=1e9+10;
+    int out=0;
+    int n;
     n=read();
-    for(register int i=1;i<=n;i++){
-        a[i]=read();
-    }
-    m=read();
-    for(register int i=1;i<=m;i++){
-        q[i].l=read();
-        q[i].r=read();
-        q[i].id=i;
-    }
-    std::sort(q+1,q+m+1);
+    a[n+1]=1e9+10;
 
-    build(1,1,n);
+    for(int i=1;i<=n;i++) a[i]=read();
     int index=1;
-    for(register int  i=1;i<=n;i++){
-        if(last[a[i]]){
-            update(1,last[a[i]]);
+    while(index<=n+1){
+        while(a[index]<a[index-1] && index<=n) index++;
+        int head=index-1,tail=index;
+        while(a[tail]>a[head]){
+            if(vis[head]){
+                head--;continue;
+            }
+            sum+=tail-head-1;
+            vis[head]=1;
+            head--;
+            if(++cnt>=n) break;
         }
-        last[a[i]]=i;
-        while(q[index].r==i){
-            ans[q[index].id]=query(1,q[index].l,q[index].r);
-            index++;
-        }
+        index++;
     }
-    for(register int i=1;i<=m;i++){
-        write(ans[i]);
-        putchar('\n');
-    }
+    cout<<sum<<endl;
     return 0;
 }
